f8e28127847e98d3540f4ac43da056204f82798b
Erlethaden <107069832+Erlethaden@users.noreply.github.com>
2026-07-17 18:09:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'reiatsu100-pwa-v3';
+var CACHE = 'reiatsu100-pwa-v4';
 var ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(e) {
